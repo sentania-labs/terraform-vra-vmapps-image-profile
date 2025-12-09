2e59966218e0d1ce6e28c35b4f4f918a79eb9749
Scott R Bowe <scottb@sentania.net>
2025-12-09 14:13:00 -0600

    update actions (#2)
    * update actions
    
    * fixing linting complaints

--- variables.tf
+++ variables.tf
@@ -1,12 +1,17 @@
 variable "region" {
+  type = string
 }
 
 variable "image_profile_name" {
+  type = string
 }
 variable "image_description" {
+  type    = string
+  default = "Created by TF - Do not edit!"
 }
 
 variable "cloud_account_id" {
+  type = string
 }
 variable "image_mappings" {
   type = list(object({
